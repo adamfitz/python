e117a5bbbceb77328051233996c7908d80975cff
Adam Fitzgerald <adam@networknoob.net>
2016-11-20 10:46:32 +1100

    added counter for the number of returned PTRs

--- subnetCheck.py
+++ subnetCheck.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
 # Author: Adam Fitzgerald
-# Purpose: Script to iterate though all IPs in an IP address block and output
-# any A records that are found
+# Purpose: Script to iterate though all IPs in an IP address block and outputs
+# the PTR records if they are found
 # Version: 1.0
 #
 # ipaddress module docs can be found here:
@@ -11,6 +11,7 @@
 # socket module docs found here:
 # https://docs.python.org/2/library/socket.html#module-socket
 #
+from __future__ import print_function
 from sys import argv
 import ipaddress
 import socket
@@ -27,10 +28,10 @@ usersIpBlock = ipaddress.ip_network(unicodeAddressBlock, strict=False)
 numberOfIpv4HostAddresses = usersIpBlock.num_addresses
 
 #Output some basic information to the screen
-print ""
-print "Your have entered the network: %s" % (addressBlock)
-print "The number of addresses in this subnet is: %d" % (numberOfIpv4HostAddresses)
-print ""
+print ("")
+print ("Your have entered the network: %s" % (addressBlock))
+print ("The number of addresses in this subnet is: %d" % (numberOfIpv4HostAddresses))
+print ("")
 
 #retrieve all the host ips from the IPv4Network class
 allHostIps = list(usersIpBlock.hosts())
@@ -40,15 +41,17 @@ ipsToIterateThrough = map(str, allHostIps)
 
 #function to do the DNS lookup and print the results to the screen
 def getDnsRecords():
+	#var to count the number of returned PTRs
+	totalNumberOfReturnedPtrRecords = 0
+
 	for i in ipsToIterateThrough:
 		try:
 			ptr_record = list(socket.gethostbyaddr(i))
-			print "%-20s %-20s" % (i, ptr_record[0])
+			print ("%-20s %-20s" % (i, ptr_record[0]))
+			totalNumberOfReturnedPtrRecords = totalNumberOfReturnedPtrRecords + 1
 		except socket.herror as unknownHostError:
 			continue
+	print ("\nThe number PTR records found is: %s, out of a potential %s" % (totalNumberOfReturnedPtrRecords, numberOfIpv4HostAddresses))
 
 # call the function
 getDnsRecords()
-
-	
-	
