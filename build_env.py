ad7f17ddff66123bd3cf5eccae4002e5d5924f19
Adam Fitzgerald <adam@networknoob.net>
2017-06-03 17:38:28 +1000

    cleaned up print statements, cleaned up help text and doc strings

--- build_env.py
+++ build_env.py
@@ -1,31 +1,23 @@
 #!/usr/bin/env python3
 
 """
-Script to setup a project with virtulenv
+Synopsis:
+---------
+A script to setup virtulenv for a new project.
 
-- creates project direectory
-- sets up virtualenv in project directory
-- activates the environments
+- creates a new project directory
+- installs a package into the a virtualenv project directory
 
-- checks if the project directory already exists
-- if directory exists the script will install libraries to that directory
-- script can be run with a -i flag (install library) and will install to the
-specified project dir
-
-Constants
-
-1 = flag for what the script should (is being told) to do
-2 = the virtualenv directory to create or where a new package will be installed
-3 = the package name to install
-
+Constants:
+----------
+CONSTANT_1 = flag to control what the script will do
+CONSTANT_2 = the virtualenv directory or the name of the directory to create
+CONSTANT_3 = the package name to install
 
 Flags:
-
+------
 -n = create a new project directory and setup virtualenv for this project
 -i = install a package into the specified virtualenv directory
--c = check if project directory exists and python3 binaries are installed in it
-
-
 """
 
 from __future__ import print_function
@@ -47,32 +39,30 @@ def build_env_help():
 	print (
 	"""
 	Name:	build_env.py script
-	=====
+	-----
 
 	Usage: -h (prints this help screen)
-	======
-
+	------
 	This script will create a new project directory and setup virtualenv
-	under this directory.  It has been tested only on linux systems (mint 18).
+	under this directory.  This script has only been tested on linux systems
+	(mint 18).
 
 	The default directory path this script uses is:
 	/home/<current user>/scripts/
 
-	New project directories will be created in the folowing location:
+	New project directories will be created in the following location:
 	"/home/<current user>/scripts/<newProjectName>"
 
-	Examples:
-	=========
+	The virtualenv project directory is:
+	"/home/<current user>/scripts/<newProjectName>/env"
 
+	Examples:
+	---------
 	Create a new virtualenv environment in <newProjectName> directory
 	$ build_env.py -n newProjectName
 
 	Install a package into the <newProjectName> virtualenv directory
 	$ build_env.py -i newProjectName packageName
-
-	Check that the required python3 binaries are installed in the virtulenv
-	<newProjectName> directory
-	$ build_env.py -c newProjectName
 	"""
 	)
 
@@ -109,10 +99,10 @@ def create_project_directory():
 	print("Checking if the project directory exists (",CONSTANT_2,"), if "\
 	"not it will be created in the following location: ", default_dir)
 	if not os.path.exists(new_directory):
-		print("\nCreated project directory: ", new_directory)
+		print("\nCreated project directory: {0}".format(new_directory))
 		os.makedirs(new_directory)
 	else:
-		print("\nDirectory already exists! (", new_directory, ")")
+		print("\nDirectory already exists! {0}".format(new_directory))
 		print("\nNo action performed...")
 	return new_directory
 
@@ -123,10 +113,11 @@ def install_package(project_directory, package_name):
 	"""
 	pip_binary = project_directory + "/env/bin/pip3"
 	# subprocess out to get the list of installed libraries
-	# read the list of installed packages
-	# convert the byte string output to unicode
-	get_virtualenv_packages = subprocess.Popen([pip_binary, 'freeze'], stdout=subprocess.PIPE)
+	get_virtualenv_packages = subprocess.Popen([pip_binary, 'freeze'], \
+	stdout=subprocess.PIPE)
+	# read list of installed packages from stdout
 	installed_packages = (get_virtualenv_packages.stdout.read()).split()
+	# convert the byte string output to unicode
 	installed_packages = [(i).decode() for i in installed_packages]
 	# check if the requested package is already installed
 	if any(package_name in packages for packages in installed_packages):
@@ -137,22 +128,20 @@ def install_package(project_directory, package_name):
 
 def setup_python3_binaries(project_directory):
 	"""
-	Check if python3 binaries are installed in a specific virtualenv directory.
+	Check if python3 binaries are installed in the specified virtualenv
+	directory.
 	"""
 	python3_inst = "n"
 	environment_dir = project_directory + "/env"
 	if not os.path.exists(project_directory + "/env/bin/python3"):
-		print("Installing python3 binaries to: \n", environment_dir)
+		print("Installing python3 binaries to: \n".format(environment_dir))
 		subprocess.call(['virtualenv', '-p', 'python3', environment_dir])
 	else:
-		print("Python3 binary already exists in the project directory ",\
-		environment_dir," no action taken...")
+		print("Python3 binary already exists in the project directory {0} no "\
+		"action taken...".format(project_directory))
 
 def main():
-	if (len(argv) < 2 or len(argv) > 4):
-		print("-*- Too many or too few paramaters... -*- ")
-		build_env_help()
-	elif CONSTANT_1 == "-h":
+	if (len(argv) < 2 or len(argv) > 4) or CONSTANT_1 == "-h":
 		build_env_help()
 	# creates new project directory
 	elif (CONSTANT_1 == "-n" and len(argv) == 3):
@@ -162,9 +151,9 @@ def main():
 	# installs a package
 	elif (CONSTANT_1 == "-i" and len(argv) == 4):
 		package_name = CONSTANT_3
-		#check_dependencies('virtualenv')
+		check_dependencies('virtualenv')
 		project_directory = create_project_directory()
-		#setup_python3_binaries(project_directory)
+		setup_python3_binaries(project_directory)
 		install_package(project_directory, package_name)
 	else:
 		build_env_help()
