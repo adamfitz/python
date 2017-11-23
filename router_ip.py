a716c25410c6f0d1149049284dc0410c5dae838e
Adam Fitzgerald <adam@networknoob.net>
2017-11-23 23:57:10 +1100

    working on extracting WAN IP

--- router_ip.py
+++ router_ip.py
@@ -16,6 +16,8 @@ Modules:
 """
 from selenium import webdriver
 from time import sleep
+from lxml import *
+from lxml import html
 
 browser = webdriver.Chrome('./chromedriver')
 router_url = "http://10.23.0.254/login.lp" # router IP addres//web portal login
@@ -33,10 +35,19 @@ login_button = browser.find_element_by_id("sign-me-in")
 login_button.click()
 
 # sleep for 5 seconds to allow the initial settings page to load
-sleep(5)
+sleep(3)
 
-# load up and grab the setting spage
+# load up and grab the settings page and assign html output to string variable
 what_is_my_ip__url = "http://10.23.0.254/modals/internet-modal.lp"
 browser.get(what_is_my_ip__url) # go to page behind the login page?
 innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a string
-print(innerHTML)
+
+# search html and convert to text
+html_element = html.document_fromstring(innerHTML)
+#print(dir(html_element.cssselect))
+div_class_element = html_element.cssselect("\"class=control-group\"")
+#element_text = []
+#for instance in div_class_element:
+    #text = instance.text_content()
+    #element_text.append(text)
+#print(type(element_text))
